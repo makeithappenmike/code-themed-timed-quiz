6402fb71c9145f23e2e246268d3eff733766e9f0
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-21 07:39:10 -0400

    got timer counting down on screen

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-// function startTimer() {
-//     setInterval(function() {
-
-//     }, 1000)
-// }
-
 function startTimer(seconds) {
+
+    // Set timer to value passed onClick
     let counter = seconds;
+
+    // Timer
     var timeAllowed = setInterval(() => {
         console.log(counter);
+
+        // document.getElementById("time-remaining").innerHTML = "3";
+
+        // Decrement timer
         counter--;
+        document.getElementById("time-remaining").innerHTML = counter + 1;
+
+        // // Grab Time Remaining
+        // var timeRemaining = document.getElementsByClassName("time-remaining").innerHTML;
+        // console.log(timeRemaining);
 
         // When counter reaches 0, stop counting down
         if (counter < 0 ) {
+            document.getElementById("time-remaining").innerHTML = counter;
             clearInterval(timeAllowed);
+            document.getElementById("time-remaining").innerHTML = "0";
             console.log("Time's up!");
+            // alert("Time's Up!");
         }
+    
 
     }, 1000);
 };
