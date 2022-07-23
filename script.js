4f1bedd5c7e2ee79623f95e3fb9c192a24101982
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-23 17:48:35 -0400

    gonna go a differen direction with an array

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function askQuestions() {
     var question3 = document.getElementById("question3");
     var question4 = document.getElementById("question4");
     var question5 = document.getElementById("question5");
+    var choice = document.getElementById("choice");
+    var answer1 = "";
+
+    const questions = [
+        {
+            question: "Question 1",
+            answers: {
+                1: "Answer1",
+                2: "Answer2",
+                3: "Answer3",
+                4: "Answer4",
+            },
+            correctAnswer: "3"
+        }
+    ];
 
     console.log(startButton);
     console.log(question1);
@@ -17,6 +32,8 @@ function askQuestions() {
     });
 
     question1.addEventListener("click", function() {
+        answer1 = question1.textContext;
+        console.log(answer1);
         question1.style.display = "none";
         question.textContent = "QUESTION 2";
         question2.style.display = "block";
